789f103cd098d2550665a8b8a8f83ce1bd5ec193
Abdullahaujla100 <abdullahaujla18@gmail.com>
2025-07-23 10:18:38 +0500

    a basic gui calculator

---
+++ copy_here.py
@@ -0,0 +1,12 @@
+words = [
+    "apple", "bridge", "cloud", "dream", "forest",
+    "grape", "honey", "island", "jungle", "knife",
+    "lemon", "mirror", "night", "orange", "pencil",
+    "quiet", "rocket", "shadow", "travel", "window",
+    "ball", "cat", "dance", "earth", "flame",
+    "giant", "house", "insect", "jelly", "kitten",
+    "light", "monkey", "narrow", "ocean", "pearl",
+    "queen", "river", "stone", "table", "uncle",
+    "village", "water", "xenon", "yogurt", "zebra",
+    "actor", "brave", "candle", "desert", "energy"
+]
